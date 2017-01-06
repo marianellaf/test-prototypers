8c5b5633b9333d779287b7d84fb9c71f0ff85bba
marianella fabrega astudillo <marianella fabrega astudillo>
2017-01-06 13:23:48 -0300

    nuevos avances nav terminado slide terminado, falta mejoras de background, falta mejoras de seccion preguntas y detalles

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 $(window).load(function() {
-2
+
   $(".btn-nav").on("click tap", function() {
-3
+
     $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
-4
+
     $(this).toggleClass("animated");
-5
-  });
-6
+ });
+
+});
+
+$(document).ready(function() {
+$('#slides').superslides({
+ slide_easing: 'easeInOutCubic',
+ slide_speed: 800,
+ pagination: true,
+ hashchange: true,
+ scrollable: true
+});
 });
